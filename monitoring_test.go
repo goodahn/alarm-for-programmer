a79ce257e1a2cdcb897e9b3a8e5aaa8a50b42586
wnaldj1589TW <wnaldj1589@twinny.co.kr>
2020-09-28 02:49:30 +0900

    [#1] Add the test case for monitoring command

---
+++ monitoring_test.go
@@ -0,0 +1,38 @@
+package alarm_test
+
+import (
+	"os/exec"
+	"sync"
+	"testing"
+	"time"
+
+	alarm "github.com/goodahn/AlarmForProgrammer"
+	"github.com/stretchr/testify/require"
+)
+
+func TestMonitoringCommand(t *testing.T) {
+	cmd := "test_monitoring_command"
+
+	commandNum := 10
+	m := alarm.NewCommandMonitor([]string{
+		cmd,
+	})
+	m.SetMonitoringPeriod(100 * time.Millisecond)
+	m.Start()
+
+	time.Sleep(50 * time.Millisecond)
+
+	wg := sync.WaitGroup{}
+	for i := 0; i < commandNum; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			c := exec.Command("bash", "test_monitoring_command.sh")
+			err := c.Run()
+			require.Nil(t, err)
+		}()
+	}
+	wg.Wait()
+
+	require.Equal(t, commandNum, m.TotalAlarmCount())
+}
