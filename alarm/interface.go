131f692e6aaa1c5668a20de4e58964d9566d17dd
goodahn <wnaldj1589@gmail.com>
2021-09-09 01:46:37 +0900

    [#11] Separate injector part of Alarmer from alarm/interface.go

--- alarm/interface.go
+++ alarm/interface.go
@@ -20,17 +20,3 @@ type Alarmer interface {
 
 	Stop()
 }
-
-func NewAlarmer(configPath string) Alarmer {
-	cm := alarm.NewConfigMonitor(configPath)
-
-	alarmConfig := cm.GetAlarmConfig()
-	if alarmConfig["type"] == "slack-webhook" {
-		am := SlackWebHookAlarmer{}
-		am.Init(cm)
-		am.Start()
-		return &am
-	} else {
-		panic("not implemented type of alarmer")
-	}
-}
